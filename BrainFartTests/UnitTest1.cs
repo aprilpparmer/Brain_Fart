63e814b98df90134f970a07d74effc108f020e65
MDammes1 <mdammes1@my.westga.edu>
2015-06-17 15:42:17 -0500

    Added Test Project and reference.  Code to be added soon

---
+++ BrainFartTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BrainFartTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
