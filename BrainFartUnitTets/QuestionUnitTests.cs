ef14a83a2448d44ff586f4034348367d4a4afbb8
MDammes1 <mdammes1@my.westga.edu>
2015-06-19 18:53:36 -0500

    Added two test folders an classes

---
+++ BrainFartUnitTets/QuestionUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BrainFart.DAL;
+using BrainFart.Model;
+using BrainFart.Controller;
+using BrainFart.DB;
+using System.Collections.Generic;
+
+namespace BrainFartUnitTests
+{
+    [TestClass]
+    public class QuestionUnitTests
+    {
+        [TestMethod]
+        public void TestGetAllQuestions()
+        {
+            List<Questions> questions = BrainFartController.GetAllQuestions();
+            bool questionsFound = questions.Count == 30;
+            Assert.AreEqual(true, questionsFound);
+        }
+
+    }
+}
+
+
