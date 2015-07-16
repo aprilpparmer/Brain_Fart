071f3e0a38f337420cb5c180f1f910e9d0871556
MDammes1 <mdammes1@my.westga.edu>
2015-07-15 22:48:46 -0500

    Added Unit Test File for Games no code yet

---
+++ BrainFartUnitTests/GamesUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using BrainFart.DAL;
+using BrainFart.Model;
+using BrainFart.Controller;
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace BrainFartUnitTests
+{
+    [TestClass]
+    public class GamesUnitTests
+    {
+
+        [TestMethod]
+        public void TestAddUserStats()
+        {
+
+        }
+    }
+}
